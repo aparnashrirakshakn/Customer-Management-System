dd33955bed1375c70b58dc86ac9a64455793c849
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-02 18:30:22 -0500

    add test for instance count of Customer

--- CustomerManagementSystem/Tests/CustomerManagementSystem.BusinessLayerTest/CustomerTests.cs
+++ CustomerManagementSystem/Tests/CustomerManagementSystem.BusinessLayerTest/CustomerTests.cs
@@ -58,6 +58,24 @@ namespace CustomerManagementSystem.BusinessLayerTest
             Assert.AreEqual(expected, actual);
         }
 
+        [Test]
+        public void InstanceCount_Returns_Correct_Instance_Count()
+        {
+            //Arrange
+            Customer customer1 = new Customer();
+            Customer.InstanceCount += 1;
+
+            Customer customer2 = new Customer();
+            Customer.InstanceCount += 1;
+
+            Customer customer3 = new Customer();
+            Customer.InstanceCount += 1;
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
 
     }
 }
