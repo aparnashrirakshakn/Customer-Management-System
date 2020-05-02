f7ca14a2ac299376e3fe8173f365b9c97f615ed4
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-02 18:04:53 -0500

    add invalid full name tests for Customer

--- CustomerManagementSystem/Tests/CustomerManagementSystem.BusinessLayerTest/CustomerTests.cs
+++ CustomerManagementSystem/Tests/CustomerManagementSystem.BusinessLayerTest/CustomerTests.cs
@@ -24,5 +24,40 @@ namespace CustomerManagementSystem.BusinessLayerTest
             Assert.AreEqual(expected, actual);
         }
 
+        [Test]
+        public void Valid_FirstName_And_No_LastName_Returns_Valid_FullName()
+        {
+            //Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Aparna"
+            };
+            string expected = "Aparna";
+
+            //Act
+            string actual = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void No_FirstName_And_Valid_LastName_Returns_Valid_FullName()
+        {
+            //Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Shriraksha"
+            };
+            string expected = "Shriraksha";
+
+            //Act
+            string actual = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
     }
 }
