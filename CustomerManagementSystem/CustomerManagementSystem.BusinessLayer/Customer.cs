22b3ada21657ed2222ee98fd117130a07624d18e
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-02 17:49:11 -0500

    create the Customer class in Business Layer

---
+++ CustomerManagementSystem/CustomerManagementSystem.BusinessLayer/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CustomerManagementSystem.BusinessLayer
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get 
+            {
+                return _lastName;
+            }
+
+            set 
+            {
+                _lastName = value;
+            }
+        }
+
+        public string FirstName { get; set; } 
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+        public string Email { get; set; } 
+
+
+    }
+
+}
+}
