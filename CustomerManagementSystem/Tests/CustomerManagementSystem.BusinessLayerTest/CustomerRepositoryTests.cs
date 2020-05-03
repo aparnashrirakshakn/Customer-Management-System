b3bd4a1d67b3bb371bfc235c489e0d5ec68724db
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-05-03 11:26:54 -0500

    write valid test for Customer Repository

---
+++ CustomerManagementSystem/Tests/CustomerManagementSystem.BusinessLayerTest/CustomerRepositoryTests.cs
@@ -0,0 +1,31 @@
+﻿using CustomerManagementSystem.BusinessLayer;
+using NUnit.Framework;
+
+namespace CustomerManagementSystem.BusinessLayerTest
+{
+    public class CustomerRepositoryTests
+    {
+        [Test]
+        public void Valid_CustomerId_Retrieves_Valid_Customer()
+        {
+            // Arrange
+            CustomerRepository customerRepository = new CustomerRepository();
+
+            var expected = new Customer(1)
+            {
+                Email = "myvalidemail@email.me",
+                FirstName = "Aparna",
+                LastName = "Shriraksha"
+            };
+
+            // Act
+            var actual = customerRepository.Retrieve(1);
+
+            // Assert
+            Assert.AreEqual(expected.CustomerId, actual.CustomerId);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+            Assert.AreEqual(expected.Email, actual.Email);
+        }
+    }
+}
